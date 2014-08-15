85d6c43823d048154db3d57cde74bd548a8b8f5a
nov <nov@matake.jp>
2014-08-15 23:38:52 +0900

    Edge#next & Edge#previous first cut

--- lib/fb_graph2/collection.rb
+++ lib/fb_graph2/collection.rb
@@ -1,9 +1,17 @@
 module FbGraph2
   class Collection < Array
-    attr_reader :previous, :next, :total_count, :unread_count, :updated_time, :cursors
+    attr_accessor :before, :after, :next, :previous
 
-    def initialize(collection)
-      collection = case collection
+    def initialize(collection = [])
+      collection = normalize collection
+      setup_pagination collection[:paging]
+      replace Array(collection[:data])
+    end
+
+    private
+
+    def normalize(collection)
+      case collection
       when Hash
         collection
       when Array
@@ -14,7 +22,24 @@ module FbGraph2
       else
         raise ArgumentError.new("Invalid collection")
       end
-      replace Array(collection[:data])
+    end
+
+    def setup_pagination(paging)
+      cursors  = paging.try(:[], :cursors)
+      self.before   = cursors.try(:[], :before)
+      self.after    = cursors.try(:[], :after)
+      self.next     = params_in paging.try(:[], :next)
+      self.previous = params_in paging.try(:[], :previous)
+    end
+
+    def params_in(url)
+      if url
+        Rack::Utils.parse_nested_query(
+          URI.parse(url).query
+        ).inject({}) do |params, (key, value)|
+          params.merge! key => value
+        end.with_indifferent_access
+      end
     end
   end
 end
