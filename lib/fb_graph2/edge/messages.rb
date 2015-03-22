46dcc87a64befa4f509a6612e10f7bc223b111cc
nov <nov@matake.jp>
2015-03-23 00:35:50 +0900

    typo

--- lib/fb_graph2/edge/messages.rb
+++ lib/fb_graph2/edge/messages.rb
@@ -2,7 +2,7 @@ module FbGraph2
   class Edge
     module Messages
       def messages(params = {})
-        messages = self.edge :conversations, params
+        messages = self.edge :messages, params
         messages.collect! do |message|
           Message.new(message[:id], message).authenticate message[:access_token]
         end
