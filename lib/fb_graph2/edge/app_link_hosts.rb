518762f7ae0089d29bac0565ca68662528e33381
nov <nov.matake@gree.net>
2015-07-07 15:52:50 +0900

    allow posting app_link_hosts

--- lib/fb_graph2/edge/app_link_hosts.rb
+++ lib/fb_graph2/edge/app_link_hosts.rb
@@ -7,6 +7,20 @@ module FbGraph2
           AppLinkHost.new(host[:id], host).authenticate self.access_token
         end
       end
+
+      def app_link_host!(params = {})
+        jsonized_params = params.inject({}) do |jsonized_params, (key, value)|
+          jsonized_param = case value
+          when Hash, Array
+            value.to_json
+          else
+            value
+          end
+          jsonized_params.merge! key => jsonized_param
+        end
+        host = self.post jsonized_params, edge: :app_link_hosts
+        AppLinkHost.new(host[:id], params.merge(host)).authenticate self.access_token
+      end
     end
   end
 end
