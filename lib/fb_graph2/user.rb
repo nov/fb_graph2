750280706165248847a69b256432a0dcfb5a45cd
nov <nov.matake@gree.net>
2015-07-13 15:47:23 +0900

    notifications and outbox edges are no longer availabe.
    https://developers.facebook.com/docs/apps/changelog#v2_4_changes
    
    ps.
    outbox isn't mentioned in the doc, but read_mailbox deprecation causes outbox deprecation too.

--- lib/fb_graph2/user.rb
+++ lib/fb_graph2/user.rb
@@ -20,9 +20,6 @@ module FbGraph2
     include Edge::Likes::LikerContext
     include Edge::Movies
     include Edge::Music
-    include Edge::Notifications
-    include Edge::OpenGraph::Actions
-    include Edge::Outbox
     include Edge::PaymentTransactions
     include Edge::Permissions
     include Edge::Picture
