b4497a0cb99d35b9fdddd06eac2ba576df5aad64
Niklas Bichinger <niklas@bichinger.de>
2015-07-14 17:48:50 +0200

    changed class/method names to reflect opengraph

--- spec/fb_graph2/edge/open_graph/actions_spec.rb
+++ spec/fb_graph2/edge/open_graph/actions_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe FbGraph2::Edge::Actions do
+describe FbGraph2::Edge::OpenGraph::Actions do
   context 'included in User' do
-    describe '#actions' do
+    describe '#og_actions' do
       let(:me) { FbGraph2::User.me('token') }
       it 'should return an Array of FbGraph2::Post' do
-        posts = mock_graph :get, 'me/og.likes', 'user/actions_og_likes', access_token: 'token' do
-          me.actions 'og.likes'
+        posts = mock_graph :get, 'me/og.likes', 'user/og_actions_likes', access_token: 'token' do
+          me.og_actions 'og.likes'
         end
         posts.should be_instance_of FbGraph2::Edge
         posts.should_not be_blank
@@ -16,11 +16,11 @@ describe FbGraph2::Edge::Actions do
       end
     end
 
-    describe '#action!' do
+    describe '#og_action!' do
       let(:user) { FbGraph2::User.new('user_id') }
       it 'should return a FbGraph2::Post' do
         mock_graph :post, 'user_id/og.likes', 'success_with_id', access_token: 'app_token' do
-          user.authenticate('app_token').action! 'og.likes', object: 'https://github.com/nov/fb_graph2/'
+          user.authenticate('app_token').og_action! 'og.likes', object: 'https://github.com/nov/fb_graph2/'
         end.should be_instance_of FbGraph2::Post
       end
     end
